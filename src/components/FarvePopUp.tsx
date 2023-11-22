0f2dcc8f1adfca3940f2d45bae45299d7e4a103a
BawarHamid <bawar@live.dk>
2023-11-22 23:26:05 +0100

    .

--- src/components/FarvePopUp.tsx
+++ src/components/FarvePopUp.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import "./FarvePopUp.scss";
 
-const colors = [
+const colorList = [
   "#7A3F98",
   "#C86EAF",
   "#5298D3",
@@ -30,32 +30,63 @@ type Props = {
 
 const FarvePopUp: React.FC<Props> = ({ onClose }) => {
   const [selectedColor, setSelectedColor] = useState<string | null>(null);
+  const [colors, setColors] = useState<string[]>(colorList);
 
   const handleSelectedColor = (color: string) => {
     setSelectedColor(color);
   };
 
+  const generateRandomHexCode = () => {
+    const hexCode = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += hexCode[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  };
+
+  const handleAddColor = () => {
+    const randomColor = generateRandomHexCode();
+    const updatedColors = [...colors, randomColor];
+    setColors(updatedColors);
+    setSelectedColor(randomColor);
+  };
+
   return (
-    <table className="color__picker">
-      <tr className="color__grid">
-        {colors.map((color, index) => (
-          <div
-            key={index}
-            className="color__option"
-            style={{ backgroundColor: color }}
-            onClick={() => handleSelectedColor(color)}
-          />
-        ))}
-      </tr>
-      <h3 className="text__selected__color">Valgte farve:</h3>
-      <div
-        className="selected__color"
-        style={{ backgroundColor: selectedColor || "white" }}
-      />
-      <button className="close__btn" onClick={onClose}>
-        Luk PopUp
-      </button>
-    </table>
+    <>
+      <table className="color__picker">
+        <tr className="color__grid">
+          {colors.map((color, index) => (
+            <div
+              key={index}
+              className="color__option"
+              style={{ backgroundColor: color }}
+              onClick={() => handleSelectedColor(color)}
+            />
+          ))}
+        </tr>
+
+        <h3 className="text__selected__color">Valgte farve:</h3>
+        <div
+          className="selected__color"
+          style={{ backgroundColor: selectedColor || "white" }}
+        />
+
+        <button className="add__color__btn" onClick={handleAddColor}>
+          Tilføj ny farve!
+        </button>
+
+        <button className="close__btn" onClick={onClose}>
+          Luk PopUp
+        </button>
+      </table>
+      <div>
+        <label className="label__1">
+          Den valgte farve: <br />
+          {selectedColor}
+        </label>
+      </div>
+    </>
   );
 };
 
